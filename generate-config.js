46bbe4b10780a7244e732612069cea9ff9c7e331
Ismael Hernández <ismael.hernandez@greenti.co>
2026-05-20 18:25:16 -0600

    udpate enviroment variables

--- generate-config.js
+++ generate-config.js
@@ -4,8 +4,10 @@ const path = require('path');
 const envPath = path.join(__dirname, '.env');
 const configPath = path.join(__dirname, 'js', 'config.js');
 
-let url = '';
-if (fs.existsSync(envPath)) {
+let url = process.env.GOOGLE_SCRIPT_URL || '';
+
+// Si no hay variable de entorno en process.env (por ejemplo en local), leemos del .env
+if (!url && fs.existsSync(envPath)) {
     const envContent = fs.readFileSync(envPath, 'utf8');
     const match = envContent.match(/GOOGLE_SCRIPT_URL=(.*)/);
     if (match) {
